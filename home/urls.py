fb04e779bedf8f3bbd34f5f0615fab06f4903436
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-18 21:51:18 +0000

    Created Urls, views and templates for remaining pages in the home app.  Templates currently duplicates of index.html

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('', views.about, name='about'),
+    path('', views.contact, name='contact'),
+    path('', views.pricing, name='pricing'),
+    path('', views.delivery, name='delivery'),
+    path('', views.assembly, name='assembly'),
+    path('', views.returns, name='returns'),
 ] 
--- home/views.py
+++ home/views.py
@@ -6,3 +6,33 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+
+    return render(request, 'home/about.html')
+
+
+def assembly(request):
+
+    return render(request, 'home/assembly.html')
+
+
+def contact(request):
+
+    return render(request, 'home/contact.html')
+
+
+def delivery(request):
+
+    return render(request, 'home/delivery.html')
+
+
+def pricing(request):
+
+    return render(request, 'home/pricing.html')
+
+
+def returns(request):
+
+    return render(request, 'home/returns.html')
